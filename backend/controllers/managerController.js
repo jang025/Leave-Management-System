5059861946c445f82301749f64d02e02a0d0e450
Jeremy Ang <jang025@users.noreply.github.com>
2025-11-18 23:08:14 +0800

    feat : modified get all leave to get all pending leave , created approve and reject leave functions and tested it on bruno

--- backend/controllers/managerController.js
+++ backend/controllers/managerController.js
@@ -1,6 +1,6 @@
 const pool = require("../config/db");
 
-const getLeaves = async (req, res) => {
+const getPendingLeaves = async (req, res) => {
   try {
     const result = await pool.query(`
       SELECT 
@@ -9,6 +9,7 @@ const getLeaves = async (req, res) => {
         u.role
       FROM leaves AS l
       INNER JOIN users AS u ON l.user_id = u.id
+      WHERE l.status = 'pending'
       ORDER BY l.created_at DESC
     `);
     res.status(200).json(result.rows);
@@ -18,4 +19,64 @@ const getLeaves = async (req, res) => {
   }
 };
 
-module.exports = { getLeaves };
+const approveLeave = async (req, res) => {
+  try {
+    const leaveId = req.params.id;
+
+    const result = await pool.query("SELECT * FROM leaves WHERE id = $1", [
+      leaveId,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ msg: "Leave request not found" });
+    }
+
+    const leave = result.rows[0];
+
+    if (leave.status !== "pending") {
+      return res
+        .status(400)
+        .json({ msg: "Only pending requests can be approved" });
+    }
+
+    await pool.query(`UPDATE leaves SET status = 'approved' WHERE id = $1`, [
+      leaveId,
+    ]);
+
+    res.status(200).json({ msg: "Leave approved successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+};
+
+const rejectLeave = async (req, res) => {
+  try {
+    const leaveId = req.params.id;
+
+    const result = await pool.query("SELECT * FROM leaves WHERE id = $1", [
+      leaveId,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ msg: "Leave request not found" });
+    }
+
+    const leave = result.rows[0];
+
+    if (leave.status !== "pending") {
+      return res
+        .status(400)
+        .json({ msg: "Only pending requests can be rejected" });
+    }
+
+    await pool.query(`UPDATE leaves SET status = 'rejected' WHERE id = $1`, [
+      leaveId,
+    ]);
+
+    res.status(200).json({ msg: "Leave rejected successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = { getPendingLeaves, approveLeave, rejectLeave };
--- backend/routes/managerRoutes.js
+++ backend/routes/managerRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getLeaves } = require("../controllers/managerController");
+const {
+  getPendingLeaves,
+  approveLeave,
+  rejectLeave,
+} = require("../controllers/managerController");
 const { auth } = require("../middleware/auth");
 const { roleCheck } = require("../middleware/roleCheck");
 const router = express.Router();
 
-router.get("/leaves", auth, roleCheck("manager"), getLeaves);
+router.get("/leaves", auth, roleCheck("manager"), getPendingLeaves);
+router.patch("/leaves/:id/approve", auth, roleCheck("manager"), approveLeave);
+router.patch("/leaves/:id/reject", auth, roleCheck("manager"), rejectLeave);
 
 module.exports = router;
