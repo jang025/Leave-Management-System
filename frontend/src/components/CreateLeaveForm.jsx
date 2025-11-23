1e8d4c4cb5751ad0e1e19912d207b64a192c31f8
Jeremy Ang <jang025@users.noreply.github.com>
2025-11-23 19:45:39 +0800

    feat : developed form for creating new leave and integrated it with the service function

--- frontend/src/components/CreateLeaveForm.jsx
+++ frontend/src/components/CreateLeaveForm.jsx
@@ -1,30 +1,50 @@
-const CreateLeaveForm = () => {
-  //! submit new leave request
-  const handleSubmit = () => {};
+import { useId } from "react";
+
+const CreateLeaveForm = ({ handleSubmit, handleChange, user }) => {
+  // useId hook is used when multiple forms exist on the same page
+  const id = useId();
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Leave Type:
-        <select>
-          <option value="annual">Annual Leave</option>
-          <option value="sick">Sick Leave</option>
-        </select>
-      </label>
+      <label htmlFor={`${id}-leaveType`}>Leave Type: </label>
+      <select
+        id={`${id}-leaveType`}
+        name="leaveType"
+        value={user.leaveType}
+        onChange={handleChange}
+        required
+      >
+        <option value="">Select Type</option>
+        <option value="annual">Annual</option>
+        <option value="sick">Sick</option>
+      </select>
 
-      <label>
-        Start Date:
-        <input type="date" />
-      </label>
-
-      <label>
-        End Date:
-        <input type="date" />
-      </label>
-
-      <label>
-        Reason:
-        <textarea placeholder="Reason for leave..." />
-      </label>
+      <label htmlFor={`${id}-startDate`}>Start Date: </label>
+      <input
+        type="date"
+        id={`${id}-startDate`}
+        name="startDate"
+        value={user.startDate}
+        onChange={handleChange}
+        required
+      />
+      <label htmlFor={`${id}-endDate`}>End Date: </label>
+      <input
+        type="date"
+        id={`${id}-endDate`}
+        name="endDate"
+        value={user.endDate}
+        onChange={handleChange}
+        required
+      />
+      <label htmlFor={`${id}-reason`}>End Date: </label>
+      <textarea
+        id={`${id}-reason`}
+        name="reason"
+        value={user.reason}
+        placeholder="Enter your reason for leave"
+        onChange={handleChange}
+        required
+      />
 
       <button type="submit">Submit</button>
     </form>
--- frontend/src/pages/CreateLeavePage.jsx
+++ frontend/src/pages/CreateLeavePage.jsx
@@ -1,8 +1,36 @@
 import { useNavigate } from "react-router";
 import CreateLeaveForm from "../components/CreateLeaveForm";
+import { useState } from "react";
+import { create } from "../services/employeeService";
 
 const CreateLeavePage = () => {
+  const token = localStorage.getItem("token");
+  const employeeId = localStorage.getItem("userId");
   const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const [user, setUser] = useState({
+    leaveType: "",
+    startDate: "",
+    endDate: "",
+    reason: "",
+  });
+
+  //! submit new leave request
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await create(user, token);
+      console.log(result);
+      navigate(`/employee/${employeeId}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleGoBack = () => {
     navigate(-1); // Goes back to the previous page
@@ -10,7 +38,11 @@ const CreateLeavePage = () => {
   return (
     <div>
       <h1>Create Leave Request</h1>
-      <CreateLeaveForm />
+      <CreateLeaveForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        user={user}
+      />
       <button onClick={handleGoBack}>Go Back</button>
     </div>
   );
